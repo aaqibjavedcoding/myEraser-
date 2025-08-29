3abce94b561ece0b2a77c138b3a7e1271526605e
TenaVishnu <vishnu.tenaciousys@gmail.com>
2025-08-29 12:57:09 +0530

    update

--- clean_migrations.py
+++ clean_migrations.py
@@ -9,3 +9,28 @@ for root, dirs, files in os.walk(project_root):
             if file != "__init__.py" and file.endswith(".py"):
                 os.remove(os.path.join(migration_path, file))
                 print("Deleted:", os.path.join(migration_path, file))
+
+
+## "for live"
+
+import os
+
+# Change this path to your project root inside the server
+project_root = "/lead/backend"
+
+for root, dirs, files in os.walk(project_root):
+    if "migrations" in dirs:
+        migration_path = os.path.join(root, "migrations")
+        for file in os.listdir(migration_path):
+            if file != "__init__.py" and file.endswith(".py"):
+                os.remove(os.path.join(migration_path, file))
+                print("Deleted:", os.path.join(migration_path, file))
+
+#Save and exit (CTRL+O, Enter, CTRL+X in nano).
+#2. Run the script with Python
+#Since your virtual environment isn’t activated yet, run it with system Python:
+#python delete_migrations.py
+
+
+# /lead/backend/venv/bin/python manage.py makemigrations
+# /lead/backend/venv/bin/python manage.py migrate --fake-initial
